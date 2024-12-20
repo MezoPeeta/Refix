e5711317b75c4eee304bbcb4441712c5631947f2
MezoPeeta <wildlifemain1@gmail.com>
2024-12-21 00:50:35 +0200

    updating web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1718017440"
+    serviceWorkerVersion: "3041348502"
   }
 });
