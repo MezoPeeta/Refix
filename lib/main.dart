0fe5770102f3ad29dd1b13a73ed22fab8cdeb17c
MezoPeeta <wildlifemain1@gmail.com>
2025-01-03 17:37:56 +0200

    adjust ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:refix/src/core/cache/helper.dart';
 import 'package:refix/src/screens/auth/domain/notification.dart';
-
+import 'dart:io';
 import 'firebase_options.dart';
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    name: Platform.isIOS ? "devproject" : null,
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await CacheHelper.init();
